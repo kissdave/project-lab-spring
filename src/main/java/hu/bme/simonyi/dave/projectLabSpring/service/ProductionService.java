ed09cc432fd7ba4e8f6a33dfebc56d4e1951b0ba
David Kiss <kiss.david@simonyi.bme.hu>
2016-07-06 13:36:54 +0200

    Test example added

---
+++ src/main/java/hu/bme/simonyi/dave/projectLabSpring/service/ProductionService.java
@@ -0,0 +1,10 @@
+package hu.bme.simonyi.dave.projectLabSpring.service;
+
+/**
+ * Created by dkiss on 2016. 07. 06..
+ */
+public class ProductionService {
+    public double TestingFunction() {
+        return 12.0;
+    }
+}
---
+++ src/test/java/hu/bme/simonyi/dave/projectLabSpring/service/ProductionServiceTest.java
@@ -0,0 +1,39 @@
+package hu.bme.simonyi.dave.projectLabSpring.service;
+
+import hu.bme.simonyi.dave.projectLabSpring.model.Production;
+import hu.bme.simonyi.dave.projectLabSpring.repository.ProductionRepository;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by dkiss on 2016. 07. 06..
+ */
+public class ProductionServiceTest {
+    public ProductionServiceTest() {
+
+    }
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Mock
+    ProductionRepository productionRepository;
+
+    @Test
+    public void testProductionService() {
+        ProductionService ps = new ProductionService();
+        ps.TestingFunction();
+        assertEquals(12.0, ps.TestingFunction(), 0.00001);
+    }
+}
